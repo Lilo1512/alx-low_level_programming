3b2cc2be549654bcb04be37abfa2d4f8e3f044fd
root <alaamohamed14101998@outlook.com>
2023-03-16 08:38:07 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+ char a;
+int b;
+long int c;
+long long int d;
+float f;
+ printf("size of a char: %lu byte(s)\n', (unsigned long)sizeof(a));
+ printf("size of a int: %lu byte(s)\n' , (unsigned long) sizeof (b));
+ printf("size of a char: %lu byte(s)\n' , (unsigned long) sizeof (c));
+ printf("size of a char: %lu byte(s)\n' , (unsigned long) size of (d));
+ printf("size of a char: %lu byte(s)\n' , (unsigned long) size of (f));
+return (0);
+}
