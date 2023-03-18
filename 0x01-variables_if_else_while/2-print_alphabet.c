622e6645aeab7d35a57fa647f43b4bee5dcc700c
root <alaamohamed14101998@outlook.com>
2023-03-17 17:15:36 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+ * main - 2 print the alphabetic
+ * Return: ALways (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z' ; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+
+}
