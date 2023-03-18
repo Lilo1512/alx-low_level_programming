0cad6e1328bbda96f018a16dad54f0d6022d84d6
root <alaamohamed14101998@outlook.com>
2023-03-17 18:03:03 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/**
+ * main - Entry Point
+ * Return: Always 0 (Success)
+ */
+
+
+int msin(void)
+
+
+
+{
+	char low, e, q;
+	e = 'e';
+	q = 'q';
+        for (low = 'a' ; low <= 'z' ; low++)
+	{
+	if (low != e && low != q)
+	}
+        putchar('\n');
+	return (0);
