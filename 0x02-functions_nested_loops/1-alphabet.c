87ffbd33cf5ccd904e12c30d2e5e400df943c66b
root <alaamohamed14101998@outlook.com>
2023-03-21 08:59:00 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Make the alphabet
+ * Return: void
+ */
+
+void print_alphabet (void)
+{
+        putchar (c);
+        for (c = 'a'; c <= 'z' ; c++)
+
+{
+        putchar(c);
+}
+        putchar('\n');
+	}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,9 @@
-int putchar(char c);
+#include <stdio.h>
+
+int ('char' c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int is lower(int c);
+int is,lower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int abs(int);
@@ -10,4 +12,4 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print times_table(int n);
+void print_times_table(int n);
