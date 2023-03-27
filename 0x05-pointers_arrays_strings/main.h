be5a3c9bbd06d9e2bcbdc878a44b94c532edf235
root <alaamohamed14101998@outlook.com>
2023-03-27 05:06:31 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,29 @@
+int _putchar(char c);
+
+int main(void);
+
+int _isupper(int c);
+
+int _isdigit(int c);
+
+int mul(int a, int b);
+
+void print_number(int n);
+
+void print_numbers(void);
+
+void print_numbers(void);
+
+void print_most_number(void);
+
+void more_numbers(void);
+
+void print_line(int n);
+
+void print_diagonal(int n);
+
+void print_square(int size);
+
+void print_triangle(int size);
+
+void reset_to_98(int *n);
