ebd377877b2d29411932d7649c7cd6590450218b
root <alaamohamed14101998@outlook.com>
2023-03-29 16:40:06 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,39 @@
+int _isdigit(int c);
+
+int mul(int a, int b);
+
+void print_number(int n);
+
+void print_numbers(void);
+
+void print_numbers(void);
+
+void print_most_number(void);
+
+void more_numbers(void);
+
+void print_line(int n);
+
+void print_diagonal(int n);
+
+void print_square(int size);
+
+void print_triangle(int size);
+
+void print_rev(char *s);
+
+void rev_string(char *s);
+
+void puts2(char *str);
+
+void reset_to_98(int *n);
+
+void puts_half(char *str);
+
+void print_array(int *a, int n);
+
+char *_strcpy(char *dest, char *src);
+
+int _atoi(char *s);
+
+char *_strcat(char *dest, char *src)
