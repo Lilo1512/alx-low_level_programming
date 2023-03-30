361898eb7fc8202f9fbd36fc6dd5a02876f93812
root <alaamohamed14101998@outlook.com>
2023-03-29 22:26:12 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -27,16 +27,17 @@ int main(void)
 void print_number(int n)
 
 {
-unsigned int n1;
-n1 = n;
-if (n < 0)
-{
-putchar('-');
-n1 = -n;
-}
-if (n1 / 10 != 0)
-{
-print_number(n1 / 10);
-}
-putchar((n1 % 10) + '0');
+
+	unsigned int n1;
+	n1 = n;
+	if (n < 0)
+	{
+	putchar('-');
+	n1 = -n;
+	}
+		if (n1 / 10 != 0)
+		{
+		print_number(n1 / 10);
+		}
+	putchar((n1 % 10) + '0');
 }
