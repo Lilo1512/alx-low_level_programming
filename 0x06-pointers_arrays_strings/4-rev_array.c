57cfc5f8fef588e58ef824af09bc52d076bf84b0
root <alaamohamed14101998@outlook.com>
2023-03-29 18:43:29 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,13 +7,13 @@
  */
 void reverse_array(int *a, int n)
 {
-	int temporary, counter;
-	n = n - 1;
-	counter = 0;
-	while (counter <= n)
+int temporary, counter;
+n = n - 1;
+counter = 0;
+while (counter <= n)
 	{
-		temporary = a[counter];
-		a[counter++] = a[n];
-		a[n--] = temporary;
+	temporary = a[counter];
+	a[counter++] = a[n];
+	a[n--] = temporary;
 	}
 }
