26b35fd06288ac24a9f8be3f770f8b25865f9f75
root <alaamohamed14101998@outlook.com>
2023-03-29 20:15:29 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,15 +9,17 @@ char *leet (char *str)
 {
 	int i;
 	int j;
+	
 	char *a = "aAeEootTIL";
 	char *b = "4433007711";
+	
 	for (i = 0; str[i] != '\0'; i++)
 	{
-	for (j = 0; a[j] != '\0'; j++)
-	{
-	if (str[i] == a[j])
-	str[i] = b[j];
-	}
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (str[i] == a[j])
+				str[i] = b[j];
+		}
 	}
 	return (str);
 }
