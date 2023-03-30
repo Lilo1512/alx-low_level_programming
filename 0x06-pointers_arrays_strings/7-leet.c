b479ff7d44088ed3bb08e3629f46ce62318328b5
root <alaamohamed14101998@outlook.com>
2023-03-29 21:08:21 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,34 +6,29 @@
  * Letters o and O are replaced by 0
  * Letters t and T are replaced by 7
  * Letters l and L are replaced by 1
- *
  * @s: pointer to string.
- *
  * Return: pointer to s.
  */
-
 char *leet(char *s)
-
 {
-
-	int string_length, leetCount;
-	char leetLetters [] = "aAeEootTIL"; 
-	char leetNums[] = "4433007711";
-	/* scan through string */
-	string_length = 0;
-	while (s[string_length] != '\0')
-	/* check whether leetLetter is found */
-	{
-	leetCount = 0;
-	while (leetCount < 10)
-	{
-	if (leetLetters [leetCount] == s[string_length])
-	{
-	s[string_length] = leetNums[leetCount];
-	}
-	leetCount++;
-	}
-	string_length++;
-	}
-	return (s);
+int string_length, leetCount;
+char leetLetters [] = "aAeEootTIL"; 
+char leetNums[] = "4433007711";
+/* scan through string */
+string_length = 0;
+while (s[string_length] != '\0')
+/* check whether leetLetter is found */
+{
+leetCount = 0;
+while (leetCount < 10)
+{
+if (leetLetters [leetCount] == s[string_length])
+{
+s[string_length] = leetNums[leetCount];
+}
+leetCount++;
+}
+string_length++;
+}
+return (s);
 }
