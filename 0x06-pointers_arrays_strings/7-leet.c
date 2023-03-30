6fdcfd247c395ffbf97df405393f39a7cfccd960
root <alaamohamed14101998@outlook.com>
2023-03-29 21:06:22 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -19,34 +19,21 @@ char *leet(char *s)
 	int string_length, leetCount;
 	char leetLetters [] = "aAeEootTIL"; 
 	char leetNums[] = "4433007711";
-
-/* scan through string */
-
+	/* scan through string */
 	string_length = 0;
 	while (s[string_length] != '\0')
-
-/* check whether leetLetter is found */
-
-{
-
+	/* check whether leetLetter is found */
+	{
 	leetCount = 0;
 	while (leetCount < 10)
-
 	{
-	
 	if (leetLetters [leetCount] == s[string_length])
-
 	{
-
 	s[string_length] = leetNums[leetCount];
-	
 	}
-	
 	leetCount++;
-	
 	}
-	
 	string_length++;
-
+	}
 	return (s);
 }
