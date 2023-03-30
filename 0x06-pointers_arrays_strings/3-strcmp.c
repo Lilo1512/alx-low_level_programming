a0487538cf355bd942a1c8ec48aeaa6678b99c8e
root <alaamohamed14101998@outlook.com>
2023-03-29 18:23:09 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,17 +12,11 @@ int _strcmp(char *s1, char *s2)
 {
 	
 	int counter, compare_value;
-
 	counter = 0;
-	
 	while (s1[counter] == s2[counter] && s1[counter] != '\0')
-	
 	{
-	counter++;
+		counter++;
 	}
-	
 	compare_value = s1[counter] - s2[counter];
-	
 	return (compare_value);
-
 }
