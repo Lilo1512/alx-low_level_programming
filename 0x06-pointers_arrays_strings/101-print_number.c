f20ed004e1925b55d6ca029819e4ef65bb216742
root <alaamohamed14101998@outlook.com>
2023-03-29 22:31:39 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,23 +7,6 @@
  * main - check the code
  * Return: 0
  */
-
-int main(void)
-
-{
-    print_number(98);
-    putchar('\n');
-    print_number(402);
-    putchar('\n');
-    print_number(1024);
-    putchar('\n');
-    print_number(0);
-    putchar('\n');
-    print_number(-98);
-    putchar('\n');
-    return (0);
-}
-
 void print_number(int n)
 
 {
