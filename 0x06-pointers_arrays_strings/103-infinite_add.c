ae435397599f908a890cb6e9c9ba398694e1c03d
root <alaamohamed14101998@outlook.com>
2023-03-30 19:24:33 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <math.h>
-
 /**
  * in string to be a buffer.
  * Assume that string are never empty and
@@ -15,12 +14,6 @@
  * @r: store result
  * @size_r: size of buffer
  * Return:returns pointer to result
- */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
-
-{
-
-/**
  * add_strings - Adds the numbers stored in two strings.
  * @n1: The string containing the first number to be added.
  * @n2: The string containing the second number to be added.
@@ -29,28 +22,21 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
  * Return: If r can store the sum a pointer to the result.
  * 	If r cannot store the sum - 0.
  */
-
-int num, tens 0;
-
-for ((*n1 && *n2); n1--, n2--, r_index--)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-num = (*n1 - '0') + (n2 - '0');
+int num, tens = 0;
+for (; n1; n1--, size_r--)
+for (; n2; n2--, size_r--)
+num = (*n1 - '0') + (*n2 - '0');
 num += tens;
-*(r + r_index) = (num % 10) + '0';
+*(r + size_r) = (num % 10) + '0';
 tens = num / 10;
-}
-
-for ((*n1); n1--; r_index++)
-{
+for (; *n1; n1--, size_r--)
 num = *(n1 - '0') + tens;
-*(r + r_index) (num % 10)+'0';
+*(r + size_r) = (num % 10) + '0';
 tens = num / 10;
-}
-for ((n2); (n2--); r_index--)
-{
+for (; n2; n2--, size_r--)
 num = (*n2 - '0') +  tens;
-*(r + r_index) = (num % 10) + "0";
+*(r + size_r) = (num % 10) + '0';
 tens = num / 10; 
 }
-}
-
