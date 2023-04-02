548c3d35f8d747d33d6725d26b74a7808db483b3
root <alaamohamed14101998@outlook.com>
2023-04-01 19:22:25 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -11,13 +11,8 @@ int main(void)
 
     print_last_digit(98);
     print_last_digit(0);
-    print_last_digit(1);
-    print_last_digit(-1024);
     r = print_last_digit(-1024);
-    putchar('0' + r);
-    putchar('\n');
-
+    _putchar('0' + r);
+    _putchar('\n');
     return (0);
-
-
 }
