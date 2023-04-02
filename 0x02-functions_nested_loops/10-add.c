abd01fb1802c89c891fea6e252cc7475951461f9
root <alaamohamed14101998@outlook.com>
2023-04-02 05:39:24 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,16 +1,11 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * add - adds two values
+ * @n1: The first number to be computed
+ * @n2: The second number to be computed
+ * Return: Integer value
  */
-int add(int, int);
+int add(int n1, int n2)
 {
-    int n;
-
-    n = add(89, 9);
-    printf("%d\n", n);
-    return (0);
+return (n1 + n2);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,6 @@
 
 void print_alphabet(void);
 int is,lower(int c);
-int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 int _putchar(char c);
@@ -14,3 +13,4 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+int add(int, int);
