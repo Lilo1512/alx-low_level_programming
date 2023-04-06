a30221718a07e0ef5dabb47d649524abc6b9b936
root <alaamohamed14101998@outlook.com>
2023-04-06 13:50:07 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,38 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+int main(void);
+
+int mul(int a, int b);
+
+void print_number(int n);
+
+void print_numbers(void);
+
+void print_numbers(void);
+
+void print_most_number(void);
+
+void more_numbers(void);
+
+void print_line(int n);
+
+void print_diagonal(int n);
+
+void print_square(int size);
+
+void print_triangle(int size);
+
+int _isupper(int c);
+
+int _isupper(int x);
+
+int _isdigit(int c);
+
+void print_array(int *a, int n);
+
+void _puts_recursion(char *s);
+
+#endif
