ed557cb52a6b3678392d3cf4b44d7e6b8428c157
root <alaamohamed14101998@outlook.com>
2023-04-11 18:41:29 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,12 +1,14 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
 *str_concat - Concatenates two strings of any size
 *@s1: the first string to concatenate
 *@s2: the second string to concatenate
 *Return: the two strings concatenated
 */
+
 char *str_concat(char *s1, char *s2)
 {
 int i = 0, j = 0, k = 0, l = 0;
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -26,5 +26,6 @@ void simple_print_buffer(char *buffer, unsigned int size);
 int main(void);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
 
 #endif
