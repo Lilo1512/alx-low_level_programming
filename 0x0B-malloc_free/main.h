7450c7dbe614a027c69cbed753656c40248b0cc7
root <alaamohamed14101998@outlook.com>
2023-04-11 20:16:23 -0700

    101-strtow.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -29,5 +29,6 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 int main(int ac, char *av[]);
+char **strtow(char *str);
 
 #endif
