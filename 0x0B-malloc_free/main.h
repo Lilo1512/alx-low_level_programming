c07736b4fe68a21b9d8eb561a14abb147f138178
root <alaamohamed14101998@outlook.com>
2023-04-11 17:32:31 -0700

    1-strdup.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -24,5 +24,6 @@ char *_strstr(char *haystack, char *needle);
 char *create_array(unsigned int size, char c);
 void simple_print_buffer(char *buffer, unsigned int size);
 int main(void);
+char *_strdup(char *str);
 
 #endif
