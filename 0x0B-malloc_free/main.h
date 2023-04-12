642c6ac44144ea9dbcd188e829308e6467b0445d
root <alaamohamed14101998@outlook.com>
2023-04-11 17:52:01 -0700

    2-str_concat.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -25,5 +25,6 @@ char *create_array(unsigned int size, char c);
 void simple_print_buffer(char *buffer, unsigned int size);
 int main(void);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 
 #endif
