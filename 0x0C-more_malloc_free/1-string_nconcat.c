bfc62835111c276b229793d8a7d30ce2c7960182
root <alaamohamed14101998@outlook.com>
2023-04-15 16:46:48 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,26 +17,28 @@ unsigned int i = 0, j = 0, k = 0, l = 0;
 char *str;
 
 if (s1 == NULL)
-	s1 = "";
+s1 = "";
 
 if (s2 == NULL)
-	s2 = "";
+s2 = "";
 
 while (s1[i])
-	i++;
+i++;
 
 while (s2[k])
-	k++;
+k++;
 
 if (n >= k)
-	l = i + k;
+{
+l = i + k;
 
 else
-	l = i + n;
+l = i + n;
 
 str = malloc(sizeof(char) * l + 1);
+}
 if (str == NULL)
-	return (NULL);
+return (NULL);
 
 k = 0;
 while (j < l)
