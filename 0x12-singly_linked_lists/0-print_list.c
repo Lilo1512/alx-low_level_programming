0b21593adb08199f0a6c263581b9c7bf773c9e3a
root <alaamohamed14101998@outlook.com>
2023-04-28 17:01:05 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * print_list - prints all the elements of a list_t list.
  * @h: singly linked list.
@@ -6,7 +7,9 @@
  */
 
 size_t print_list(const list_t *h)
+
 {
+
 size_t nelem;
 nelem = 0;
 while (h != NULL)
