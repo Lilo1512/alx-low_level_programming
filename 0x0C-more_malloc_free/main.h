e42513b83d180fb1431767c255fc7aad389b7f18
root <alaamohamed14101998@outlook.com>
2023-04-30 18:18:05 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -32,5 +32,6 @@ char **strtow(char *str);
 void *malloc_checked(unsigned int b);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif
