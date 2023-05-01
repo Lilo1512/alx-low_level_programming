d2ce40500d8fddc964f19668e1c44b47e915b204
root <alaamohamed14101998@outlook.com>
2023-04-30 17:53:14 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-main.c
+++ 0x0C-more_malloc_free/2-main.c
@@ -10,25 +10,26 @@
  *
  * Return: Nothing.
  */
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-    unsigned int i;
 
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
+void simple_print_buffer(char *buffer, unsigned int size)
+
+{
+unsigned int i;
+i = 0;
+while (i < size)
+{
+if (i % 10)
+{
+printf(" ");
+}
+if (!(i % 10) && i)
+{
+printf("\n");
+}
+printf("0x%02x", buffer[i]);
+i++;
+}
+printf("\n");
 }
 
 /**
@@ -36,15 +37,16 @@ void simple_print_buffer(char *buffer, unsigned int size)
  *
  * Return: Always 0.
  */
-int main(void)
-{
-    char *a;
 
-    a = _calloc(98, sizeof(char));
-    strcpy(a, "Best");
-    strcpy(a + 4, " School! :)\n");
-    a[97] = '!';
-    simple_print_buffer(a, 98);
-    free(a);
-    return (0);
+int main(void)
+
+{
+char *a;
+a = _calloc(98, sizeof(char));
+strcpy(a, "Best");
+strcpy(a + 4, " School! :)\n");
+a[97] = '!';
+simple_print_buffer(a, 98);
+free(a);
+return (0);
 }
