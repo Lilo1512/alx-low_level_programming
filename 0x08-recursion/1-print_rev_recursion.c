5351a0e8e76c6c344bc703a84fc959d417fe101e
root <alaamohamed14101998@outlook.com>
2023-06-09 08:12:57 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * _print_rev_recursion - Print a string in reverse
  * *@s: the string to reverse
  * Return: Nothing.
  */
+
 void _print_rev_recursion(char *s)
 {
 if (*s == '\0')
